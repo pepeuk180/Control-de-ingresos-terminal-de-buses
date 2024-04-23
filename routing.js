21a818675c1b222a98c5c148d06fdb84bef054a8
ppuk-medina <Jose.Medina@fundacion-jala.org>
2024-04-22 23:41:31 -0400

    api-express-basico

---
+++ routing.js
@@ -0,0 +1,24 @@
+const http = require('node:http')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/':
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>Mi pagina</h1>')
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404</h1>')
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
